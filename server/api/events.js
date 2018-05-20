cbae6a70a1a12bf5da18f431c7f23a5eef4abc71
odan <odan3240@gmail.com>
2018-05-20 09:58:08 +0900

    fix typo

--- server/api/events.js
+++ server/api/events.js
@@ -4,18 +4,18 @@ const router = Router();
 
 const events = [
   {
-    title: 'ビリヤードをしよう！', name: 'スポーツ', place: '麻布十番', date: '2018/05/20', number: '10', level: { suport: '1', paresis: 'one_hand' }, type: '1',
+    title: 'ビリヤードをしよう！', name: 'スポーツ', place: '麻布十番', date: '2018/05/20', number: '10', level: { support: '1', paresis: 'one_hand' }, type: '1',
 
   },
   {
-    title: 'テニスをしよう！', name: 'スポーツ', place: '秋葉原', date: '2018/05/22', number: '10', level: { suport: '2', paresis: 'one_hand' }, type: '1',
+    title: 'テニスをしよう！', name: 'スポーツ', place: '秋葉原', date: '2018/05/22', number: '10', level: { support: '2', paresis: 'one_hand' }, type: '1',
 
   },
   {
-    title: '映画を観に行こう', name: '芸術', place: '新宿', date: '2018/05/23', number: '10', level: { suport: '1', paresis: 'half_body' }, type: '2',
+    title: '映画を観に行こう', name: '芸術', place: '新宿', date: '2018/05/23', number: '10', level: { support: '1', paresis: 'half_body' }, type: '2',
   },
   {
-    title: '美術館に行こう！', name: '芸術', place: '千代田', date: '2018/05/24', number: '10', level: { suport: '1', paresis: 'two_hand' }, type: '2',
+    title: '美術館に行こう！', name: '芸術', place: '千代田', date: '2018/05/24', number: '10', level: { support: '1', paresis: 'two_hand' }, type: '2',
   },
 ];
 
